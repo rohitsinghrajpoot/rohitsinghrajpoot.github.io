760a3ff592fc4eb8d11295e240b541ceb9974dde
Rohit Singh <95858652+rohitsinghrajpoot@users.noreply.github.com>
2023-03-31 18:27:31 +0530

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("#resumeButton");
-
-const handleResumeClick = () =>{
-    window.open("https://drive.google.com/file/d/14j5vC9e8RQBBQUd5xUX7oZfS_i2QYEvp/view" , "_blabk" );
-}
-
-
-button.addEventListener("click" , handleResumeClick );
