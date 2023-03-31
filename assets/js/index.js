eba16b2f3152e144ef8d951a71971d0313af31d4
Rohit Singh <95858652+rohitsinghrajpoot@users.noreply.github.com>
2023-03-31 16:05:26 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("#resumeButton");
+
+const handleResumeClick = () =>{
+    window.open("https://drive.google.com/file/d/14j5vC9e8RQBBQUd5xUX7oZfS_i2QYEvp/view" , "_blabk" );
+}
+
+
+button.addEventListener("click" , handleResumeClick );
